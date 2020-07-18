6ddcdf6b9adfed3613b3cfda40012d4b2b098fe8
sanya6456 <66361881+sanya6456@users.noreply.github.com>
2020-07-18 11:30:15 +0200

    api request practice (didn't work on responsive look)

---
+++ index.js
@@ -0,0 +1,29 @@
+const api={
+    key:'73c53d8938670e8f15c862fdbf9b757e',
+    base:'https://api.openweathermap.org/data/2.5/'
+}
+
+var obj={
+    city:''
+}
+
+
+function search(){
+
+    obj.city=document.getElementById('inputtxt').value;
+
+    var cityfield=document.getElementById('city');
+    var tempfield=document.getElementById('temp');
+    var moodfield=document.getElementById('mood');
+
+    fetch(`${api.base}weather?q=${obj.city}&units=metric&appid=${api.key}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+
+        cityfield.innerHTML=data['name'];
+        tempfield.innerHTML=data['main']['temp'];
+        moodfield.innerHTML=data['weather']['0']['main'];
+    });
+
+}
